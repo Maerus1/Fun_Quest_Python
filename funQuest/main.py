5e87bac6891304bdab2b0e80419fad5664680fa3
Maerus1 <danieldorsey472@gmail.com>
2015-10-16 19:42:28 -0400

    Revert "Re-establishing files to repository"
    This reverts commit e4780275a1278022507938f8ae25cdba871f2b45.

---
+++ funQuest/main.py
@@ -0,0 +1,10 @@
+import Character
+
+'''
+This will be the main game, the game will launch from here and I will also
+do my testing here.
+'''
+
+player = Character.Player("Dan", "Katana")
+
+enemy1 = Character.Enemy("Wolf")
